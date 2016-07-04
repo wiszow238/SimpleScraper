5d817e6bcdd4b78d9b492a8c2e00a1f6e44cf50e
David Wiszowaty <dwiszowa@purdue.edu>
2016-07-03 19:12:56 -0500

    Added Script file. Added minor changes

--- emcChallenge/emcScraper.py
+++ emcChallenge/emcScraper.py
@@ -2,6 +2,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from emcChallenge.spiders.emcSpider import genericSpider
 from urllib.request import urlopen
+from urllib import error
 
 import tldextract
 import json
@@ -9,21 +10,32 @@ import json
 if __name__ == "__main__":
     weburl = None
     depth = 0
+    print("Type 'exit' to quit")
     while True:
         # Get Starting link
-        inp = input("Enter URL you wish to craw ('http://www.example.com'): ")
+        inp = input("Enter URL you wish to crawl ('http://www.example.com'): ")
+        if inp == "exit":
+            exit(1)
         try:
             weburl = urlopen(inp)
-        except ValueError:
+        except (ValueError, error.HTTPError, error.URLError):
             print("Invalid URL address. Please enter again\n")
             continue
+        break
 
+    while True:
         # Get how deep the spider should go in the network
         depth = input("Enter how deep the spider should go: ")
-        depth = int(depth)
-        while depth <= 0:
-            depth = input("Invalid depth\n"
-                          "Enter how deep the spider should go: ")
+        if depth == "exit":
+            exit(1)
+        try:
+            depth = int(depth)
+        except ValueError:
+            print("Invalid Input")
+            continue
+        if depth <= 0:
+            print("Invalid depth")
+            continue
         break
 
     # Create Dictionary which stores visited website as key and internal links on page as the values
@@ -54,3 +66,4 @@ if __name__ == "__main__":
           "Writing results to JSON file")
     with open('CrawlerResults.json', 'w') as fp:
         json.dump(visitedPages, fp)
+    print("Finished web crawler")
